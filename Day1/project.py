f118dd3cbc0003c495875d51fae380794e2ec9a0
Manish Mishra <manishsmishra97@gmail.com>
2022-06-14 13:33:16 +0530

    added Day 1 project: Band Name Generator

---
+++ Day1/project.py
@@ -0,0 +1,9 @@
+# Day 1 project
+# Band Name generator
+
+print("Welcome to the Band Name Generator.")
+city_name = input("What's name of the city you grew up in? ")
+print(city_name)
+pet_name = input("What's your pet name? ")
+print(pet_name)
+print("Your band name could be {0} {1}".format(city_name,pet_name))
