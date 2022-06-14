e7b17c1035eb3c22297ee0ed7fe609ecb85b7f85
Manish Mishra <manishsmishra97@gmail.com>
2022-06-14 13:40:01 +0530

    removed extra print statements

--- Day1/project.py
+++ Day1/project.py
@@ -2,8 +2,9 @@
 # Band Name generator
 
 print("Welcome to the Band Name Generator.")
-city_name = input("What's name of the city you grew up in? ")
-print(city_name)
-pet_name = input("What's your pet name? ")
-print(pet_name)
+
+city_name = input("What's name of the city you grew up in? \n")
+
+pet_name = input("What's your pet name? \n")
+
 print("Your band name could be {0} {1}".format(city_name,pet_name))
