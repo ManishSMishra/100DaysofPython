4249116c05bc7db1d89b0f1691e3b60ca9b1e3cb
Manish Mishra <manishsmishra97@gmail.com>
2022-07-12 12:30:46 +0530

    Updated Day26 Project: Added Exception Handling

--- Day26/main.py
+++ Day26/main.py
@@ -7,11 +7,21 @@ data = pandas.read_csv("Day26\\nato_phonetic_alphabet.csv")
 
 
 phonetic_dict = {row.letter:row.code for (index,row) in data.iterrows()}
-# print(nato_disct)
-
 
 user_word=list(input("Enter a word: ").upper())
-# print(user_word)
+
+def check(user_word):
+    try:
+        for word in user_word:
+            phonetic_dict[word]
+    except:
+        print("Sorry, only letters in alphabet please")
+        return False
+    else:
+        return True
+
+while not check(user_word):
+    user_word=list(input("Enter a word: ").upper())
 
 nato_list=[phonetic_dict[letter] for letter in user_word]
 print(nato_list)
